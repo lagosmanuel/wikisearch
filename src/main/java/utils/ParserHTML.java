05930b062ec7c2907642c7cf6c4ec185a0ee1f37
lagosmanuel <lagosmanual@gmail.com>
2024-06-05 23:47:20 -0300

    refactor utils

--- src/main/java/utils/ParserHTML.java
+++ src/main/java/utils/ParserHTML.java
@@ -10,12 +10,11 @@ public class ParserHTML {
         int score = searchResult.getScore();
         String lastmodified = searchResult.getLastmodified();
 
-        String starsString = score>0?UIStrings.STAR_CHAR_FULL.repeat(score)+UIStrings.STAR_CHAR_EMPTY.repeat(UIStrings.SCORE_MAXSCORE - score)+" ":"";
-        String rateHTML = "<span style=\"color: #e3c153; font-size: 18px;\">"+starsString+"</span><br/>";
-        String titleHTML = "<span style=\"color: white; font-weight: bold; font-size: 12px; font-family: arial;\">"+title+"</span>";
+        String starsHTML = "<span style=\"color: #e3c153; font-size: 18px;\">"+scoreToStarString(score)+"</span><br/>";
+        String titleHTML = "<span style=\"color: white; font-weight: bold; font-size: 12px;\">"+title+"</span>";
         String snippetHTML = "<span style=\"font-weight: normal;\">"+snippet+"</span>";
-        String lastmodifiedHTML= lastmodified != null?"<br><span style=\"font-weight: normal;\">"+UIStrings.RANKINGVIEW_LASTMODIFIED_LABEL+lastmodified+"</span><br/>":"";
-        String searchResultHTML = "<html>"+rateHTML+titleHTML+": "+snippetHTML+lastmodifiedHTML+"</html>";
+        String lastmodifiedHTML= lastmodifiedStringToHTML(lastmodified);
+        String searchResultHTML = "<html>"+starsHTML+" "+titleHTML+": "+snippetHTML+lastmodifiedHTML+"</html>";
 
         return searchResultHTML.replace("<span class=\"searchmatch\">", "")
                                .replace("</span>", "");
@@ -24,7 +23,7 @@ public class ParserHTML {
     public static PageResult formatPageResult(PageResult pageResult) {
         return pageResult.setExtract(
             "<font face=\"arial\">"
-                + (pageResult.hasThumbnail()?"<img src=\"%s/%d\">".formatted(UIStrings.IMAGECACHE_BASEURL, pageResult.getPageID()):"")
+                + getPageResultImageHTML(pageResult)
                 + "<h1>" + pageResult.getTitle() + "</h1>"
                 + pageResult.getExtract().replace("\\n", "\n")
                 + "<a href=\"%s\">%s</a>".formatted(pageResult.getUrl(), UIStrings.PAGE_LINK_MSG)
@@ -36,5 +35,20 @@ public class ParserHTML {
         return
             "p, a { font-size: 18px; margin: 0px; }" +
             " h1 { font-size: 30px; margin: 0px; }";
+    }
+
+    private static String scoreToStarString(int score) {
+        return score > 0 && score <= UIStrings.SCORE_MAXSCORE?
+            UIStrings.STAR_CHAR_FULL.repeat(score)+UIStrings.STAR_CHAR_EMPTY.repeat(UIStrings.SCORE_MAXSCORE-score):"";
+    }
+
+    private static String lastmodifiedStringToHTML(String lastmodified) {
+        return lastmodified != null?
+            "<br><span style=\"font-weight: normal;\">"+UIStrings.RANKINGVIEW_LASTMODIFIED_LABEL+lastmodified+"</span>":"";
+    }
+
+    private static String getPageResultImageHTML(PageResult pageResult) {
+        return pageResult.hasThumbnail()?
+                "<img src=\"%s/%d\">".formatted(UIStrings.IMAGECACHE_BASEURL, pageResult.getPageID()):"";
     }
 }
